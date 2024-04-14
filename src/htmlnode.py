cdaf35fdc9d63315252c3aab8d5a062334aa7dc3
KvattS <kristoffer.skantz@outlook.com>
2024-04-14 22:24:28 +0200

    started on a parentnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,4 +33,12 @@ class LeafNode(HTMLNode):
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
 
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children == "":
+            raise ValueError("Invalid HTML: no children")
