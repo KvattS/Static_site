2ebad881f8f53dbec4dfbe39ad5e16d07791b4ad
KvattS <kristoffer.skantz@outlook.com>
2024-04-15 21:54:46 +0200

    finished the ParentNode Class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -38,7 +38,14 @@ class ParentNode(HTMLNode):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
-        if self.tag == None:
+        if self.tag is None:
             raise ValueError("Invalid HTML: no tag")
-        if self.children == "":
+        if self.children is None:
             raise ValueError("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
