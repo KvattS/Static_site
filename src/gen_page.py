94966a199c2d4fed56d397dea40d0c32eeaaafc2
KvattS <kristoffer.skantz@outlook.com>
2024-05-17 23:55:14 +0200

    recursive site generation added

--- src/gen_page.py
+++ src/gen_page.py
@@ -1,6 +1,22 @@
 import os
 from markdown_blocks import markdown_to_html_node
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    content_dir = os.listdir(path=dir_path_content)
+    for content in content_dir:
+        if os.path.isfile(os.path.join(dir_path_content, content)):
+            generate_page(
+                os.path.join(dir_path_content, content),
+                template_path,
+                os.path.join(dest_dir_path, "index.html")
+                )
+        else:
+            os.mkdir(os.path.join(dest_dir_path, content))
+            generate_pages_recursive(
+                   os.path.join(dir_path_content, content),
+                   template_path,
+                   os.path.join(dest_dir_path, content)
+                   )
 
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
